208cd64db8b288d4bd2334b4ea2937d0176a28b3
Yassir aittata <yassir.aittata@gmail.com>
2024-02-04 22:23:02 +0000

    init function

---
+++ main.py
@@ -0,0 +1,18 @@
+class Item:
+    def __init__(self, title: str, price: float, quantity=0):
+        # validation
+        assert price >= 0, f"price must be positif number"
+        assert quantity >= 0, f"quantity must be positif number"
+
+        # assign to theself object
+        self.title = title
+        self.price = price
+        self.quantity = quantity
+
+    def calculate_price(self):
+        return
+
+
+phone = Item('phone', 5, 4)
+
+print(phone.title)
